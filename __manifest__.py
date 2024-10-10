6d8b3530910a185f089aef74c5f164c7d9fa5a7a
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2024-10-10 16:41:21 -0600

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Edicion_facturav16',
-    'version': '16.1',
+    'version': '16.2',
     'license': 'AGPL-3',
     'author': 'Demsa Industrial',
     'website': '',
