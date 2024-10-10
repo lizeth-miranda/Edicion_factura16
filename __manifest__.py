0c8393cf706556526251b5b7d1b5b85bf29c8d5a
lizeth-miranda <67765437+lizeth-miranda@users.noreply.github.com>
2024-10-10 16:23:09 -0600

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -5,17 +5,20 @@
     'author': 'Demsa Industrial',
     'website': '',
     'depends': [
-        'account_accountant',
+        'account_accountant','account', 'account_payment', 'l10n_mx_edi',
 
 
     ],
     'data': [
         # security
+        'security/ir.model.access.csv',
         # data
         # demo
         # reports
         # views
         'views/account_move.xml',
+        'views/multi_payment_views.xml',
+        'wizards/multi_payments.xml',
 
     ],
     'license': 'LGPL-3',
